70cc0217f5a545934bbc0e7d75cbae7b43723e8a
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2023-08-20 16:40:32 +0600

    calculation done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let total = 0
+// let discount = total - 20%
 function cardClick(target){
     const itemContainer = document.getElementById('selectItem')
     const itemName = target.parentNode.childNodes[3].childNodes[3].childNodes[3].innerText
@@ -8,7 +9,16 @@ function cardClick(target){
 
     const price = target.parentNode.childNodes[3].childNodes[3].childNodes[5].innerText.split(' ')[0]
     total = parseFloat(total) + parseFloat(price);
-    document.getElementById("totalPrice").innerText = total;
-    // console.log(target.parentNode.childNodes[3].childNodes[3].childNodes[5].innerText.split(' ')[0])
+    document.getElementById("totalPrice").innerText = total.toFixed(2);
+    // discount
+    if(total >= 200){
+        const discount = total * 0.20;
+        const finalTotal = total - discount;
+        
+        document.getElementById("discount").innerText = discount.toFixed(2);
+        document.getElementById("finalTotal").innerText = finalTotal.toFixed(2);
+    }
+   
+
     
 }
