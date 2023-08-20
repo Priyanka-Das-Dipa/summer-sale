135f693c4941c9de69f4b70ae82ffa90266f2a0f
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2023-08-20 23:50:30 +0600

    pursches btn done

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
 let total = 0
-// let discount = total - 20%
 function cardClick(target){
+    // set card title
     const itemContainer = document.getElementById('selectItem')
-    const itemName = target.parentNode.childNodes[3].childNodes[3].childNodes[3].innerText
+    const itemName = target.querySelector('.card-title').innerText
     const li = document.createElement('li')
     li.innerText = itemName
     itemContainer.appendChild(li)
-
-    const price = target.parentNode.childNodes[3].childNodes[3].childNodes[5].innerText.split(' ')[0]
-    total = parseFloat(total) + parseFloat(price);
+    // set totalPrice
+    const price = parseFloat(target.querySelector('p span').innerText);
+    total += price;
     document.getElementById("totalPrice").innerText = total.toFixed(2);
+
+    // set button disable
+    const btnPurchase = document.getElementById("btnPurchase")
+    if(total > 0){
+        btnPurchase.disabled = false
+    }
+    else{
+        btnPurchase.disabled = true
+    }
     // discount
-    if(total >= 200){
+    /* if(total >= 200){
         const discount = total * 0.20;
         const finalTotal = total - discount;
         
         document.getElementById("discount").innerText = discount.toFixed(2);
         document.getElementById("finalTotal").innerText = finalTotal.toFixed(2);
+    } */
+  
+}
+function applyCoupon() {
+    const couponInput = document.getElementById("couponInput");
+    const couponCode = couponInput.value;
+    const applyBtn = document.getElementById('applyBtn')
+    if(total >= 200){
+        applyBtn.disabled = false
+        if (couponCode === "SELL200") {
+            const discount = total * 0.20;
+            const finalTotal = total - discount;
+    
+            document.getElementById("discount").innerText = discount.toFixed(2);
+            document.getElementById("finalTotal").innerText = finalTotal.toFixed(2);
+        }
+        couponInput.value = "";
     }
-   
-
+    else{
+        applyBtn.disabled = true
+    }
     
 }
