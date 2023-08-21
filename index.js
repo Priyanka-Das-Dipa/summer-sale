8f729fa24700ebadd388506fbf66576f6b228bbe
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2023-08-21 12:58:38 +0600

    button enable disable done

--- index.js
+++ index.js
@@ -1,51 +1,55 @@
-let total = 0
-function cardClick(target){
-    // set card title
-    const itemContainer = document.getElementById('selectItem')
-    const itemName = target.querySelector('.card-title').innerText
-    const li = document.createElement('li')
-    li.innerText = itemName
-    itemContainer.appendChild(li)
-    // set totalPrice
-    const price = parseFloat(target.querySelector('p span').innerText);
-    total += price;
-    document.getElementById("totalPrice").innerText = total.toFixed(2);
+let total = 0;
+const applyBtn = document.getElementById("applyBtn");
+function cardClick(target) {
+  // set card title
+  const itemContainer = document.getElementById("selectItem");
+  const itemName = target.querySelector(".card-title").innerText;
+  const li = document.createElement("li");
+  li.innerText = itemName;
+  itemContainer.appendChild(li);
+  // set totalPrice
+  const price = parseFloat(target.querySelector("p span").innerText);
+  total += price;
+  document.getElementById("totalPrice").innerText = total.toFixed(2);
 
-    // set button disable
-    const btnPurchase = document.getElementById("btnPurchase")
-    if(total > 0){
-        btnPurchase.disabled = false
+  // set button disable
+  const btnPurchase = document.getElementById("btnPurchase");
+  if (total > 0) {
+    btnPurchase.disabled = false;
+    btnPurchase.style.backgroundColor = "#E527B2";
+    if (total >= 200) {
+      applyBtn.style.backgroundColor = "#E527B2";
     }
-    else{
-        btnPurchase.disabled = true
-    }
-    // discount
-    /* if(total >= 200){
+  } else {
+    btnPurchase.disabled = true;
+  }
+  // discount
+  /* if(total >= 200){
         const discount = total * 0.20;
         const finalTotal = total - discount;
         
         document.getElementById("discount").innerText = discount.toFixed(2);
         document.getElementById("finalTotal").innerText = finalTotal.toFixed(2);
     } */
-  
 }
+
+
 function applyCoupon() {
-    const couponInput = document.getElementById("couponInput");
-    const couponCode = couponInput.value;
-    const applyBtn = document.getElementById('applyBtn')
-    if(total >= 200){
-        applyBtn.disabled = false
-        if (couponCode === "SELL200") {
-            const discount = total * 0.20;
-            const finalTotal = total - discount;
-    
-            document.getElementById("discount").innerText = discount.toFixed(2);
-            document.getElementById("finalTotal").innerText = finalTotal.toFixed(2);
-        }
-        couponInput.value = "";
+  const couponInput = document.getElementById("couponInput");
+  const couponCode = couponInput.value;
+  console.log(couponCode);
+
+  if (total >= 200) {
+    // applyBtn.disabled = false;
+    if (couponCode === "SELL200") {
+      const discount = total * 0.2;
+      const finalTotal = total - discount;
+      applyBtn.style.backgroundColor = "#ddd";
+      document.getElementById("discount").innerText = discount.toFixed(2);
+      document.getElementById("finalTotal").innerText = finalTotal.toFixed(2);
     }
-    else{
-        applyBtn.disabled = true
-    }
-    
+    couponInput.value = "";
+  } else {
+    applyBtn.disabled = true;
+  }
 }
